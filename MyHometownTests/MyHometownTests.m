e198c295cb1aa45cdbf734eaac5b609d28d210a7
Alex Gutiérrez <alex.gutierrez@avantica.net>
2013-10-18 18:31:20 -0500

    Initial Commit

---
+++ MyHometownTests/MyHometownTests.m
@@ -0,0 +1,34 @@
+//
+//  MyHometownTests.m
+//  MyHometownTests
+//
+//  Created by Alex Gutierrez on 10/18/13.
+//  Copyright (c) 2013 PoC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyHometownTests : XCTestCase
+
+@end
+
+@implementation MyHometownTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
